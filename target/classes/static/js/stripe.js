bd5ee26015c5f2b32c89b39519a50f7300c1dd41
JTS <151023119+jhonstons@users.noreply.github.com>
2024-06-22 12:44:19 +0900

    Delete target directory

--- target/classes/static/js/stripe.js
+++
@@ -1,8 +0,0 @@
- const stripe = Stripe('pk_test_51OWufQCP2Ja3niUVdBZbvCJvgxwk7vCwSWj20bkNK2lseRhR5odBvVucP5wbIZpUtDZoj1iiLEZSYTlA6AdzjiQ700EF3biv31');
- const paymentButton = document.querySelector('#paymentButton');
- 
- paymentButton.addEventListener('click', () => {
-   stripe.redirectToCheckout({
-     sessionId: sessionId
-   })
- });
